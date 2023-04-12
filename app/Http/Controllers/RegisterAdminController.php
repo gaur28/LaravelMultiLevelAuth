59abeb625dbabcd21ac78dd5719f37eeaec44a4e
Tushar Gaur <gaurtushar5567@gmail.com>
2023-04-12 17:42:58 +0530

    larvel-learning multilevel auth

---
+++ app/Http/Controllers/RegisterAdminController.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\View\View;
+
+class RegisterAdminController extends Controller
+{
+      /**
+     * Display the registration view.
+     */
+    public function create(): View
+    {
+        return view('admin.auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'roleId' => ['required','integer']
+        ]);
+
+        $admin = Admin::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'roleId'=> $request->roleId
+        ]);
+
+        event(new Registered($admin));
+
+        Auth::login($admin);
+
+        return redirect(RouteServiceProvider::HOME);
+    }
+
+}
---
+++ routes/admin.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+use function Termwind\render;
+
+
+
+Route::get('/', function () {
+    return redirect('/dashboard');
+})->name('index');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/adminAuth.php';
